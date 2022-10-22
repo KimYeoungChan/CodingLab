822f7874b28a839301e1ac755a32229886ddccde
= <=>
2022-10-22 16:17:59 +0900

    [Feat] :ab: reponsive_01

--- script.js
+++ script.js
@@ -3,12 +3,15 @@
 let menuBtn = document.querySelector(".menu-btn");
 let cancelBtn = document.querySelector(".cancel-btn");
 let navBar = document.querySelector(".navbar");
+let body = document.querySelector("body");
+
 
 // 메뉴버튼 함수
 menuBtn.onclick = function(){
   menuBtn.style.opacity = "0";
   menuBtn.style.pointerEvents = "none";
   navBar.classList.add("active");
+  body.style.overflow = "hidden"
 }
 
 // 닫기버튼 함수 
@@ -16,6 +19,7 @@ cancelBtn.onclick = function(){
   menuBtn.style.opacity = "1";
   menuBtn.style.pointerEvents = "auto";
   navBar.classList.remove("active");
+  body.style.overflow = "auto"
 }
 
 // Sticky Naviagation Menu Js Code
@@ -30,3 +34,14 @@ window.onscroll = function() {
     nav.classList.remove("sticky");
   }
 }
+
+// Slide Navigation
+let navLinks = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinks.length; i++ ){
+  navLinks[i].addEventListener("click", () => {
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+    navBar.classList.remove("active");
+    body.style.overflow = "auto"
+  });
+} 
